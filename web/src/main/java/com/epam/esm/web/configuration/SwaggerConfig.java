386a67e9e2bff61c8657858000400f6a0cec056a
nikit <nikita.kuzmichenko@gmai.com>
2022-07-26 22:03:05 +0300

    added .properties for swagger configuration

--- web/src/main/java/com/epam/esm/web/configuration/SwaggerConfig.java
+++ web/src/main/java/com/epam/esm/web/configuration/SwaggerConfig.java
@@ -1,13 +1,14 @@
 package com.epam.esm.web.configuration;
 
+import java.util.Collections;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -15,14 +16,55 @@ import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
-import java.util.List;
-
 @Configuration
+@PropertySource(value = "classpath:swagger.properties")
 @EnableSwagger2
-public class SwaggerConfig{
+public class SwaggerConfig {
 
-	public static final String AUTHORIZATION_HEADER = "Authorization";
+	@Value("${swagger.apiInfo.title:}")
+	private String apiInfoTitle;
+
+	@Value("${swagger.apiInfo.description:}")
+	private String apiInfoDesc;
+
+	@Value("${swagger.apiInfo.version:}")
+	private String apiInfoVersion;
+
+	@Value("${swagger.apiInfo.termsOfService.url:}")
+	private String apiInfoTermsOfServiceUrl;
+
+	@Value("${swagger.apiInfo.contacts.name:}")
+	private String apiInfoContactsName;
+
+	@Value("${swagger.apiInfo.contacts.url:}")
+	private String apiInfoContactsUrl;
+
+	@Value("${swagger.apiInfo.contacts.email:}")
+	private String apiInfoContactsEmail;
+
+	@Value("${swagger.apiInfo.license}")
+	private String apiInfoLicense;
+
+	@Value("${swagger.apiInfo.license.url:}")
+	private String apiInfoLicenseUrl;
+
+	@Value("${swagger.apiInfo.vendorExtensions:}")
+	private String[] apiInfoVendorExtensions;
+
+	@Value("${swagger.authorization.scope.name:}")
+	private String authorizationScopeName;
+
+	@Value("${swagger.authorization.scope.description:}")
+	private String authorizationScopeDesc;
+
+	@Value("${swagger.apiKey.headerName:}")
+	private String apiKeyHeaderName;
+
+	@Value("${swagger.apiKey.keyName:}")
+	private String apiKeyName;
+
+	@Value("${swagger.apiKey.passKeyAs:}")
+	private String apiKeyPassing;
 
 	@Bean
 	public Docket api() {
@@ -37,39 +79,45 @@ public class SwaggerConfig{
 	}
 
 	private ApiInfo apiInfo() {
-		return new ApiInfo("Certificate shop Rest APIs",
-				"APIs for application",
-				"1.NaN",
-				"",
-				null,
-				"License of API, don't have any",
-				"",
-				Collections.emptyList()
-		);
+		Contact contact;
+
+		if (apiInfoContactsName.isEmpty()
+				&& apiInfoContactsUrl.isEmpty()
+				&& apiInfoContactsEmail.isEmpty()) {
+			contact = null;
+		} else {
+			contact = new Contact(apiInfoContactsName, apiInfoContactsUrl, apiInfoContactsEmail);
+		}
+
+		return new ApiInfo(
+				apiInfoTitle,
+				apiInfoDesc,
+				apiInfoVersion,
+				apiInfoTermsOfServiceUrl,
+				contact,
+				apiInfoLicense,
+				apiInfoLicenseUrl,
+				Collections.emptyList());
 	}
 
 	private ApiKey apiKey() {
-		return new ApiKey(AUTHORIZATION_HEADER, "JWT", "header");
+		return new ApiKey(apiKeyHeaderName, apiKeyName, apiKeyPassing);
 	}
 
 	private SecurityContext securityContext() {
-		return SecurityContext.builder()
-				.securityReferences(defaultAuth())
-				.build();
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
 	}
 
 	List<SecurityReference> defaultAuth() {
-		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope authorizationScope =
+				new AuthorizationScope(authorizationScopeName, authorizationScopeDesc);
 		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
 		authorizationScopes[0] = authorizationScope;
-		return List.of(new SecurityReference(AUTHORIZATION_HEADER, authorizationScopes));
+		return List.of(new SecurityReference(apiKeyHeaderName, authorizationScopes));
 	}
 
 	@Bean
 	UiConfiguration uiConfig() {
-		return UiConfigurationBuilder.builder()
-				.defaultModelsExpandDepth(-1)
-				.build();
+		return UiConfigurationBuilder.builder().defaultModelsExpandDepth(-1).build();
 	}
-
 }
